adb0d747e84da9c5d18467b3654798e2bf95dc4f
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-25 23:27:24 +0000

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -13,6 +13,11 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+
+    this.setState({
+      text: '',
+    })
+
   }
 
   handleChange(event) {
